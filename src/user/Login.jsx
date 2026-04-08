26cca874e95015146b24a36da8fae7eaba3280ba
bibizyano18 <artem.study18@gmail.com>
2026-04-08 23:15:25 +0300

    Добавил чтение смс

--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -5,7 +5,7 @@ import './Login.css'
 export const Login = () => {
 
 	const [phone, setPhone] = useState('')
-	const [phase, setPhase] = useState('getPhone')
+	const [phase, setPhase] = useState('phone')
 
 	const handlePhoneSubmit = (e) => {
 		e.preventDefault();
@@ -19,14 +19,40 @@ export const Login = () => {
 		const formData = new FormData(e.target);
 		const code = formData.get('code');
 		console.log('Отправка кода:', { phone, code });
-
 	};
 	if (phase === 'sendCode') {
 		return (
 			<>
 				<h1>Подтверждение</h1>
-				<form className="login-form" onSubmit={handleCodeSubmit}>
+				<form onSubmit={handleCodeSubmit}>
+					<div className="input">
+						<p>Код отправлен на номер <strong>{phone}</strong></p>
+						<label htmlFor="code">
+							Введите код из SMS:<br />
+						</label>
 
+						<input
+							type="text"
+							id="code"
+							name="code"
+							pattern="[0-9]{4}"
+							placeholder="1234"
+							required
+							autoFocus
+						/>
+						<div className="form-buttons">
+							<button type="submit" className="login-button">
+								Подтвердить
+							</button>
+							<button
+								type="button"
+								className="login-button"
+								onClick={() => setPhase('phone')}
+							>
+								Назад
+							</button>
+						</div>
+					</div>
 				</form>
 			</>
 		);
@@ -37,7 +63,7 @@ export const Login = () => {
 			<form onSubmit={handlePhoneSubmit}>
 				<div className="input">
 					<label htmlFor="phone">
-						Введите номер телефона:<br/>
+						Введите номер телефона: 8(812)456-78-90<br/>
 					</label>
 
 					<input
